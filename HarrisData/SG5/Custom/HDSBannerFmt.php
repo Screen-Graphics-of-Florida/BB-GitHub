92f8f8b3f03026da89a692ef2eb886f5584263e1
bbuschsg <bbusch@screen-graphics.com>
2026-06-16 10:39:14 -0400

    Auto-sync Tue 06/16/2026 10:39:14.67

--- HarrisData/SG5/Custom/HDSBannerFmt.php
+++
@@ -1,9 +0,0 @@
-<?php
-print "\n <table width=\"100%\" cellpadding=\"5\" cellspacing=\"0\" border=\"0\">";
-print "\n <tr>";
-print "\n <td style=\"text-align: left;\">";
-print "<img src=\"{$imagePath}/SGFL338x112.gif\" alt=\"Screen Graphics\" border=\"0\">";
-print "\n </td>";
-print "\n </tr>";
-print "\n </table>";
-?>
--- HarrisData/SG5/Custom/IncludePath.php
+++
@@ -1,3 +0,0 @@
-<?php
-set_include_path(get_include_path() . PATH_SEPARATOR . '/HarrisData/SG5/custom:/HarrisData/SG5/:/HarrisData/SG5/icon:/HarrisData/SG5/images');
-?>
